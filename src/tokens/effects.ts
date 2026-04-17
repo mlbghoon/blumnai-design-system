8bac5204ef944d26e40f5568604517ab4a438978
moodzmz <74337388+moodzmz@users.noreply.github.com>
2026-04-17 16:16:06 +0900

    Adjust shadow card token

--- src/tokens/effects.ts
+++ src/tokens/effects.ts
@@ -11,7 +11,7 @@ import { color } from './color';
 
 export const effects = {
   'global-shadows': {
-    card: `0px 1px 2px 0px rgba(0, 0, 0, 0.05), inset 0px -1px 0px 0px rgba(0, 0, 0, 0.1)`,
+    card: `rgba(0, 0, 0, 0.05) 0px 1px 1px, inset 0px -1px 0px rgba(0, 0, 0, 0.10)`,
     'modal-sm': `0px 1px 1px -0.5px rgba(0, 0, 0, 0.04), 0px 3px 3px -1.5px rgba(0, 0, 0, 0.04), 0px 6px 6px -3px rgba(0, 0, 0, 0.04), 0px 12px 12px -6px rgba(0, 0, 0, 0.04), 0px 0px 0px 1px ${color.border.default}`,
     'modal-md': `0px 0px 0px 1px ${color.border.default}, 0px 12px 12px 0px rgba(0, 0, 0, 0.04), 0px 6px 6px 0px rgba(0, 0, 0, 0.04), 0px 3px 3px 0px rgba(0, 0, 0, 0.04), 0px 1px 1px -0.5px rgba(0, 0, 0, 0.04)`,
     'modal-lg': `0px 0px 0px 1px ${color.border.default}, 0px 16px 16px 0px rgba(0, 0, 0, 0.04), 0px 12px 12px 0px rgba(0, 0, 0, 0.04), 0px 6px 6px 0px rgba(0, 0, 0, 0.04), 0px 3px 3px 0px rgba(0, 0, 0, 0.04), 0px 1px 1px -0.5px rgba(0, 0, 0, 0.04)`,
